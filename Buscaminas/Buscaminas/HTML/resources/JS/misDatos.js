88fd9883a9296ab140b0afe62f711926d9cda6bc
Alexander-Sokar <160120684+Alexander-Sokar@users.noreply.github.com>
2025-12-30 18:16:45 +0100

    cambios

--- Buscaminas/Buscaminas/HTML/resources/JS/misDatos.js
+++ Buscaminas/Buscaminas/HTML/resources/JS/misDatos.js
@@ -1,24 +1,20 @@
 import { supabase } from './Supabaseclient.js';
 
-        document.addEventListener("DOMContentLoaded", async () => {
-            const user = supabase.auth.user();
-            if (!user) {
-                alert("No hay usuario logueado");
-                window.location.href = "login.html";
-                return;
-            }
+document.addEventListener("DOMContentLoaded", async () => {
 
-            const { data, error } = await supabase
-                .from('Usuarios')
-                .select('*')
-                .eq('password', user.id)
-                .single();
+    const { data: { user }, error: userError } = await supabase.auth.getUser();
 
-            if (error || !data) {
-                alert("No se pudieron cargar tus datos");
-                return;
-            }
+    const { data, error } = await supabase
+        .from('Usuarios')
+        .select('*')
+        .eq('password', user.id)
+        .single();
 
-            document.getElementById("nombreUsuario").value = data.nombre;
-            document.getElementById("passwordUsuario").value = "********";
-        });
+    if (error || !data) {
+        alert("No se pudieron cargar tus datos");
+        return;
+    }
+
+    document.getElementById("nombreUsuario").value = data.nombre;
+    document.getElementById("passwordUsuario").value = "********";
+});
