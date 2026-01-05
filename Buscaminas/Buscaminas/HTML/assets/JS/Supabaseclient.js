39a681c782c89e391aedecd4514c034d48553bb2
Alexander-Sokar <160120684+Alexander-Sokar@users.noreply.github.com>
2026-01-05 12:24:36 +0100

    cv

--- Buscaminas/Buscaminas/HTML/assets/JS/Supabaseclient.js
+++
@@ -1,8 +0,0 @@
-import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm';
-
-const supabaseURL = import.meta.env.VITE_SUPABASE_URL;
-const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
-
-console.log(supabaseURL);
-
-export const supabase = createClient(supabaseURL, supabaseKey);
