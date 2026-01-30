fa656da881dbd1aa4dda0181507eecb34a3a4c26
= <=>
2026-01-30 14:50:05 +0100

    p

--- Buscaminas/Buscaminas/api/DeleteUser.js
+++ Buscaminas/Buscaminas/api/DeleteUser.js
@@ -1,7 +1,7 @@
 const { createClient } = require('@supabase/supabase-js');
 
 const supabaseURL = "https://domodruincjgomrjrbis.supabase.co";
-const supabaseServiceKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRvbW9kcnVpbmNqZ29tcmpyYmlzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQyNzI4NzEsImV4cCI6MjA3OTg0ODg3MX0.EVoICyBq7TzChL-475wb36XlRuK7JdKRDX_XMavoGGI";
+const supabaseServiceKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRvbW9kcnVpbmNqZ29tcmpyYmlzIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc2NDI3Mjg3MSwiZXhwIjoyMDc5ODQ4ODcxfQ.oCu0YGYHkimkY9-TqYkNsHTbOn6JGnJNM9KwhMw46NM";
 
 // Crear cliente admin para backend
 const supabase = createClient(supabaseURL, supabaseServiceKey);
